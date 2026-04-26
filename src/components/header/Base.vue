e164777ff3522f6408a119d5a8245323c86a08a3
Victor <shulgasv1ls74.ru@mail.ru>
2026-04-26 17:37:30 +0500

    header second

---
+++ src/components/header/Base.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="header_base header__container">
+    <div>
+      <img src="" alt="">
+
+    </div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
